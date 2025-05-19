01a85c9b0a9d80aa2672fa3c22034409e719d17d
rohzzn <rohansanjeev6464@gmail.com>
2025-05-19 13:52:45 +0530

    Update ALLOWED_HOSTS and CORS settings in settings.py to include additional hosts for local testing and Docker integration.

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -36,6 +36,10 @@ ALLOWED_HOSTS = [
     "172.16.8.101",  # EC2 internal IP
     "doc-ai.rarediseasesnetwork.org",  # Production domain
     "*",  # Allow all hosts temporarily for troubleshooting
+    'doc-ai.rarediseasesnetwork.org', #
+    'django',                         # Docker service name for internal Nginx
+    'localhost',                      # If accessing directly on host for testing
+    '127.0.0.1',
 ]
 
 CSRF_TRUSTED_ORIGINS = [
@@ -51,15 +55,14 @@ CSRF_TRUSTED_ORIGINS = [
     "https://172.16.8.101",
     "http://doc-ai.rarediseasesnetwork.org",
     "https://doc-ai.rarediseasesnetwork.org",
+    
 ]
 
 # Make CORS more permissive for development
-CORS_ALLOW_ALL_ORIGINS = True
-# CORS_ALLOWED_ORIGINS = [
-#     "http://localhost:3000",
-# ]
-
-# Add CORS_ALLOW_CREDENTIALS
+CORS_ALLOWED_ORIGINS = [
+    "http://doc-ai.rarediseasesnetwork.org",
+    "https://doc-ai.rarediseasesnetwork.org",
+]
 CORS_ALLOW_CREDENTIALS = True
 
 # Add CORS_ALLOW_METHODS
